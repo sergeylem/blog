80cbeaa950592531c7b7c48499dbf38ceb7d5628
Sergey Lem <sergey.lem@gmail.com>
2019-10-12 23:35:27 +0600

    fixed mistakes

--- src/prisma.js
+++ src/prisma.js
@@ -5,6 +5,104 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{id name email posts {id title} }').then((data) => {
-    console.log(data)
-})
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// prisma.query.users(null, '{ id name email posts { id title } }').then((data) => {
+//     console.log(data)
+// })
+
+// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+
+// prisma.query prisma.mutation prisma.subscription prisma.exists
+
+// const createPostForUser = async (authorId, data) => {
+//     const userExists = await prisma.exists.User({ id: authorId })
+
+//     if (!userExists) {
+//         throw new Error('User not found')
+//     }
+
+//     const post = await prisma.mutation.createPost({
+//         data: {
+//             ...data,
+//             author: {
+//                 connect: {
+//                     id: authorId
+//                 }
+//             }
+//         }
+//     }, '{ author { id name email posts { id title published } } }')
+    
+//     return post.author
+// }
+
+// createPostForUser('ck1k5b5pm009t0895wis7rube', {
+//     title: 'Great books to read',
+//     body: 'The War of Art with asyn',
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+//     console.log(error.message)
+// })
+
+// prisma.mutation.createPost({
+    //     data: {
+        //         title: "GraphQL 102",
+        //         body: "it's a test",
+        //         published: false,
+        //         author: {
+            //             connect: {
+                //                 id: "ck1k5b5pm009t0895wis7rube"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then((data) => {
+//     console.log(data)
+//     return prisma.query.users(null, '{ id name posts { id title } }')
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// prisma.mutation.updatePost({
+//     where: {
+//         id: "ck1lkrfut00ak0995nur2zy6v"
+//     },
+//     data: {
+//         body: "This is how to get started with Graphql...",
+//         published: true
+//     }
+// }, '{ id }').then((data) => {
+//     return prisma.query.posts(null, '{ id title body published }')
+// }).then((data) => {
+//     console.log(data)
+// })
+
+
+// const updatePostForUser = async (postId, data) => {
+//     const postExists = await prisma.exists.Post({ id: postId })
+
+//     if (!postExists) {
+//         throw new Error('Post not found')
+//     }
+
+//     const post = await prisma.mutation.updatePost({
+//         where: {
+//             id: postId
+//         },
+//         data
+//     }, '{ author { id name email posts { id title published } } }')
+    
+//     return post.author
+// }
+
+// updatePostForUser("power", { published: true }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+//     console.log(error.message)
+// })
